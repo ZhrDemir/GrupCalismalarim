6a17b381ee9f9a4e3398fdd2fe26a6a218450e2f
ZhrDemir <zdemir2702@gmail.com>
2023-10-17 01:32:47 +0300

    20231017

---
+++ src/EfehanSorulari/sinavNotlari.java
@@ -0,0 +1,35 @@
+package EfehanSorulari;
+
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class sinavNotlari {
+
+       /*
+    Kullanıcının girdiği öğrenci sayısı ve kullanıcının girdiği toplam
+    soru sayısına bağlı olarak, öğrencilerin soruya verdiği cevapları
+    tutan bir matris düzenleyin. Bu değerleri rastgele olarak olusturun.
+    Değerler A,B,C,D,E olmalı.
+    Bir de doğru cevapları tutan tek boyutlu bir diziniz olsun. Bu da
+    rastgele oluşturulmalı.
+    Sonrasında bu değerleri karşılaştırarak her bir öğrencinin kaç doğru
+    ve yanlışı olduğunu gösteren programı yazınız.
+     */
+       public static void main(String[] args) {
+
+           Scanner scanner = new Scanner(System.in);
+           System.out.println("Lütfen öğrenci sayısını giriniz ");
+           int ogrsayi=scanner.nextInt();
+           System.out.println("Toplam soru sayısı ");
+           int soruSayisi=scanner.nextInt();
+
+
+
+
+
+
+
+
+
+       }
+}
